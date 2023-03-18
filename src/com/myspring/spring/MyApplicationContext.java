5fcf10533a1cc852e4dce437ec8361ad902ce2da
xingkeruan <xingkeruan@Xingkes-MacBook-Air.local>
2023-03-17 18:11:12 -0700

    3.BeanDefinition 生成

--- src/com/myspring/spring/MyApplicationContext.java
+++ src/com/myspring/spring/MyApplicationContext.java
@@ -2,9 +2,11 @@ package com.myspring.spring;
 
 import java.io.File;
 import java.net.URL;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MyApplicationContext {
     private Class configClass;
+    private ConcurrentHashMap<String, BeanDefinition> concurrentHashMap = new ConcurrentHashMap();
     public MyApplicationContext(Class configClass) {
         this.configClass = configClass;
 
@@ -31,7 +33,17 @@ public class MyApplicationContext {
                         try {
                             Class<?> clazz = classLoader.loadClass(className);
                             if(clazz.isAnnotationPresent(Component.class)){
+                                BeanDefinition beanDefinition = new BeanDefinition();
+                                beanDefinition.setType(clazz.getClass());
 
+                                if(clazz.isAnnotationPresent(Scope.class)){
+                                    beanDefinition.setScope(clazz.getAnnotation(Scope.class).value());
+                                }
+                                else{
+                                    beanDefinition.setScope("singleton");
+                                }
+                                String beanName = clazz.getAnnotation(Component.class).value();
+                                concurrentHashMap.put(beanName, beanDefinition);
                             }
                         } catch (ClassNotFoundException e) {
                             throw new RuntimeException(e);
@@ -42,6 +54,7 @@ public class MyApplicationContext {
         }
     }
     public Object getBean(String beanName) {
+
         return null;
     }
 }
